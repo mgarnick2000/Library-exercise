092e19b43b50f80621c7a9d4f3f25bc8549d04e0
Mark <mgarnick@techtonicgroup.com>
2018-07-10 12:20:21 -0600

    create rating feature in add books modal add rating and synopsis to books.

--- js/Book.js
+++ js/Book.js
@@ -1,6 +1,8 @@
-var Book = function(title, author, numberOfPages, publishDate) {
+var Book = function(title, author, numberOfPages, publishDate, synopsis, rating) {
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.publishDate = new Date(publishDate.toString()).getUTCFullYear();
+  this.synopsis = synopsis;
+  this.rating = rating;
 };
