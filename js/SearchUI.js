d4ec3750fba96b9adf84cd936e4407a1d0b094fc
Mark <mgarnick@techtonicgroup.com>
2018-08-01 10:44:32 -0600

    setup search results load icon.

--- js/SearchUI.js
+++ js/SearchUI.js
@@ -13,29 +13,39 @@ SearchUI.prototype._bindEvents = function () {
   $('#search-btn').on('click', $.proxy(this._handleSearch, this))
   // $('#cover-bk-img').on('click', $.proxy(this.openResults, this));
   $('#return-lib-btn').on('click', $.proxy(this.returnLibrary, this));
-  $('#search-btn').on('click', $.proxy(this._searchLoadIcon, this));
+
+
 
   return false;
 
 };
 SearchUI.prototype._searchLoadIcon = function() {
-  var searchLoad =
-  $('#search-btn').attr("search", "load").addClass('loader');
+  var div = document.createElement('div');
+  $(div).attr("id", "load-btn");
+  $(div).addClass('loader search-progress')
+  var searchLoad = $('#search-form').append(div);
+
 
 }
 
 SearchUI.prototype._handleSearch = async function (args) {
   // args.preventDefault();
+  this._searchLoadIcon()
   var searchInput = this.$container.find('#exampleInputSearch').val();
   var searchResults = await this.search(searchInput);
-  var stopSearch = $('#search-btn').attr("search", "load").removeClass('loader');
+  $('#load-btn').remove();
+
   // args.preventDefault()
   this.handlerTrigger('searchUpdate', searchResults)
   // args.preventDefault()
   $('#search-form')[0].reset()
   return;
-
   }
+
+
+
+
+
 SearchUI.prototype.returnLibrary = function () {
   console.log('happened!');
   window.location.reload(true);
